cb2ea0d9f1ddc43e5198a7644aecbb2bcd6980a1
Michal Kunert <mkunert@virtuslab.com>
2024-07-28 12:15:18 +0200

    feat: add logo

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,9 @@
+import Image from 'next/image';
+import logoSvg from '@/app/logo.svg';
 import { redirect } from 'next/navigation';
 import { createClient } from '@/shared/utils/supabase/server';
 import { SignOut } from '@/widgets/sign-out';
+import Link from 'next/link';
 
 export default async function DashboardLayout({
   children,
@@ -18,10 +21,16 @@ export default async function DashboardLayout({
     <div className="min-h-screen">
       <nav className="flex">
         <div className="container flex gap-4 mx-auto py-2 px-4">
-          <h1>Logo</h1>
-          <ul className="flex gap-4 ml-auto">
-            <li>Team</li>
-            <li>Organisation</li>
+          <h1>
+            <Image src={logoSvg} alt="Logo" width={32} height={32} />
+          </h1>
+          <ul className="flex items-center gap-4 ml-auto">
+            <li>
+              <Link href="/dashboard/team">Team</Link>
+            </li>
+            <li>
+              <Link href="/dashboard/organisation">Organisation</Link>
+            </li>
           </ul>
           <SignOut />
         </div>
